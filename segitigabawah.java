6afca52d32ebc997ea63f8b3e8b00416d3808885
Muhammad_Rivan_Surya <rvn234@gmail.com>
2026-01-19 10:36:38 +0700

    Add files via upload

---
+++ segitigabawah.java
@@ -0,0 +1,17 @@
+public class segitigabawah {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int i, j;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (j = n - i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
